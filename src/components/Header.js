0930c026d7aabcf6751e00d1e06a86d5328752e3
AminaChakirDev <amina.aitm@gmail.com>
2022-07-26 14:14:57 +0100

    Add welcome message as props

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = ({name}) => {
+    return (
+        <div>
+            <h1>Checkpoint 2 {name}</h1>
+        </div>
+    );
+};
+
+export default Header;
